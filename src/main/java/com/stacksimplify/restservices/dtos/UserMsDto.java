8c1eeb6859a4ae90a2d84ef0b986e7b5a2705d61
yguionnet <yannick.guionnet@guiodesv.fr>
2021-11-30 18:52:55 +0100

    MappStruct first commit

---
+++ src/main/java/com/stacksimplify/restservices/dtos/UserMsDto.java
@@ -0,0 +1,52 @@
+package com.stacksimplify.restservices.dtos;
+
+public class UserMsDto {
+
+	
+	private Long id;
+	private String username;
+	private String emailaddress;
+	private String rolename;
+	
+	public UserMsDto(Long id, String username, String emailaddress, String rolename) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.emailaddress = emailaddress;
+		this.rolename = rolename;
+	}
+	
+	
+	public UserMsDto() {
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailaddress() {
+		return emailaddress;
+	}
+	public void setEmailaddress(String emailaddress) {
+		this.emailaddress = emailaddress;
+	}
+
+
+	public String getRolename() {
+		return rolename;
+	}
+
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+}
---
+++ src/main/java/com/stacksimplify/restservices/mappers/UserMapper.java
@@ -0,0 +1,24 @@
+package com.stacksimplify.restservices.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import com.stacksimplify.restservices.dtos.UserMsDto;
+import com.stacksimplify.restservices.entities.User;
+
+@Mapper(componentModel = "Spring")
+public interface UserMapper {
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	@Mappings({
+		@Mapping(source= "email", target= "emailaddress"),
+		@Mapping(source= "role", target= "rolename")
+	})
+	
+	UserMsDto userToUserMsDto(User user);
+	
+	List<UserMsDto> usersToUserDtos(List<User> users);
+}
