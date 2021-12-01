30c48d4f16bfda1e52771c9f27dba2d47ac240bc
yguionnet <yannick.guionnet@guiodesv.fr>
2021-12-01 20:06:21 +0100

    Hateoas first commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -11,9 +11,11 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import org.springframework.hateoas.RepresentationModel;
+
 @Entity
 @Table(name = "user")
-public class User {
+public class User extends RepresentationModel<User>{
 	
 	@Id
 	@GeneratedValue
