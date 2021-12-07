439b80f8239b7d62a74f149dca236faf4410fe48
yguionnet <yannick.guionnet@guiodesv.fr>
2021-12-07 14:32:51 +0100

    Integration MySql with ENV

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into user values(101, 'New York','kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
-insert into user values(102, 'New Jersey','gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
-insert into user values(103, 'California', 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark');    
-insert into orders values( 2001, 'order11', 101);        
-insert into orders values( 2002, 'order12', 101);
-insert into orders values( 2003, 'order13', 101);
-insert into orders values( 2004, 'order21', 102);
-insert into orders values( 2005, 'order22', 102);
-insert into orders values( 2006, 'order31', 103);
+insert into usermgmt.user values(101, 'New York','kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
+insert into usermgmt.user values(102, 'New Jersey','gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into usermgmt.user values(103, 'California', 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark');    
+insert into usermgmt.orders values( 2001, 'order11', 101);        
+insert into usermgmt.orders values( 2002, 'order12', 101);
+insert into usermgmt.orders values( 2003, 'order13', 101);
+insert into usermgmt.orders values( 2004, 'order21', 102);
+insert into usermgmt.orders values( 2005, 'order22', 102);
+insert into usermgmt.orders values( 2006, 'order31', 103);
